089d36a1dd4880de0a6f99f8d7b66d19e58c9994
Mazen Mohamed <kidkever@yahoo.com>
2021-01-14 21:52:26 +0200

    first commit

---
+++ backend/routes/productRoutes.js
@@ -0,0 +1,19 @@
+import express from "express";
+import {
+  getProducts,
+  getProductById,
+  deleteProduct,
+  createProduct,
+  updateProduct,
+} from "../controllers/productController.js";
+import { protect, admin } from "../middleware/authMiddleware.js";
+
+const router = express.Router();
+
+router.get("/", getProducts);
+router.post("/", protect, admin, createProduct);
+router.get("/:id", getProductById);
+router.put("/:id", protect, admin, updateProduct);
+router.delete("/:id", protect, admin, deleteProduct);
+
+export default router;
---
+++ backend/routes/userRoutes.js
@@ -0,0 +1,27 @@
+import express from "express";
+import {
+  authUser,
+  deleteUser,
+  getUserById,
+  getUserProfile,
+  getUsers,
+  registerUser,
+  updateUser,
+  updateUserProfile,
+} from "../controllers/userController.js";
+import { protect, admin } from "../middleware/authMiddleware.js";
+
+const router = express.Router();
+
+router.post("/", registerUser);
+router.post("/login", authUser);
+
+router.get("/profile", protect, getUserProfile);
+router.put("/profile", protect, updateUserProfile);
+
+router.get("/", protect, admin, getUsers);
+router.get("/:id", protect, admin, getUserById);
+router.put("/:id", protect, admin, updateUser);
+router.delete("/:id", protect, admin, deleteUser);
+
+export default router;
